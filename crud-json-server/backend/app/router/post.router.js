25426e8ea771cc45b0a51e4350bc0b52d67355b0
Fang Lu Lim <fanglulim96@gmail.com>
2022-06-04 13:18:43 -0700

    backend / frontend

---
+++ crud-json-server/backend/app/router/post.router.js
@@ -0,0 +1,9 @@
+module.exports = function (app) {
+  var books = require("../controllers/book.controller.js");
+
+ app.post("/api/post", books.createBook);
+ app.get("/api/post/:id", books.getBook);
+ app.get("/api/posts", books.books);
+ app.put("/api/post/:id", books.updateBook);
+ app.delete("/api/post/:id", books.deleteBook);
+};
